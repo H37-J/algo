f054e507f058faaaa4226a3161a97de44d17d6c2
hjk <these990712@gmail.com>
2025-04-28 23:39:59 +0900

    commit

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,2 +1,55 @@
-window.onload = () => {
-}
+
+(function (global, factory) {
+
+    "use strict";
+
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = global.document ?
+            factory(global, true) :
+            function (w) {
+                if (!w.document) {
+                    throw new Error("jQuery requires a window with a document");
+                }
+                return factory(w);
+            };
+    } else {
+        factory(global);
+    }
+
+})(typeof window !== "undefined" ? window : this, function (window, noGlobal) {
+    const document = window.document;
+
+    sessionStorage.setItem('user', JSON.stringify({
+        id: 1,
+        name: 'hjk'
+    }))
+
+
+    const custom = {
+        test: () => {
+            console.log('test')
+        },
+
+        setSessionItem: (key, value) => {
+            sessionStorage.setItem(key, JSON.stringify(value))
+        },
+
+        getSessionItem: (key) => {
+            return JSON.parse(sessionStorage.getItem(key))
+        }
+    }
+
+    const data = {
+        'user' : 'name'
+    }
+
+    custom.setSessionItem('user', data)
+
+    const item = custom.getSessionItem('user')
+    console.log(item)
+
+
+    window.custom = custom;
+
+    return custom;
+})
